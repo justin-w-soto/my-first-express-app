668dc05a44bdeeec482d7d89d48b551b9370fd43
Justin W Soto <soto@pdx.edu>
2021-08-10 15:28:29 -0700

    all bugs fixed, added more datta

--- data.js
+++ data.js
@@ -1,23 +1,63 @@
 const data = [
     {
         id: 1,
-        name: 'Cowboy Dude'
+        name: 'Cowboy Dude',
+        eats_pizza: false,
+        likes_to_party: true
     },
     {
         id: 2,
-        name: 'Cowboy Dude2'
+        name: 'Cowboy Dude2',
+        eats_pizza: true,
+        likes_to_party: true
     },
     {
         id: 3,
-        name: 'Biker Dude'
+        name: 'Biker Dude',
+        eats_pizza: true,
+        likes_to_party: true
     },
     {
         id: 4,
-        name: 'Pizza Dude'
+        name: 'Pizza Dude',
+        eats_pizza: true,
+        likes_to_party: true
     },
     {
         id: 5,
-        name: 'Skater Dude'
+        name: 'Skater Dude',
+        eats_pizza: true,
+        likes_to_party: true
+    },
+    {
+        id: 6,
+        name: 'Happy Dude',
+        eats_pizza: true,
+        likes_to_party: false
+    },
+    {
+        id: 7,
+        name: 'Sad Dude2',
+        eats_pizza: false,
+        likes_to_party: false
+    },
+    {
+        id: 8,
+        name: 'Surfer Dude',
+        eats_pizza: true,
+        likes_to_party: true
+    },
+    {
+        id: 9,
+        name: 'Little Dude',
+        eats_pizza: false,
+        likes_to_party: false
+    },
+    {
+        id: 10,
+        name: 'Skater Dude',
+        eats_pizza: true,
+        likes_to_party: true
     }
 ];
 module.exports = data;
