f72d516b917e99b6daaeed8982dd6120f6ba53e3
Justin W Soto <soto@pdx.edu>
2021-08-10 13:54:59 -0700

    added data

---
+++ data.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        id: 1,
+        name: 'Cowboy Dude'
+    },
+    {
+        id: 2,
+        name: 'Cowboy Dude2'
+    },
+    {
+        id: 3,
+        name: 'Biker Dude'
+    },
+    {
+        id: 4,
+        name: 'Pizza Dude'
+    },
+    {
+        id: 5,
+        name: 'Skater Dude'
+    }
+];
+module.exports = data;
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 console.log('hello');
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const data = require('./data.js');
 
 app.get('/', (req, res) => {
   res.send('Hello Dudes!')
 });
 
 app.get('/dudes', (req, res) => {
-    res.send(`what's up dudes? ${req.query.name}`);
+    res.json(data);
 });
 
 app.listen(port, () => {
